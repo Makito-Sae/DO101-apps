73d58697c32f2e1ccac203236ddfd1db44a34879
Makito-Sae <mj.barreras24@info.uas.edu.mx>
2025-05-30 00:42:32 -0400

    update to v2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/Mars', function (req, res) {
+  res.send('Hello Mars, Maria Jose Barreras Castro!\n');
+});
+
+app.get('/Pluton', function (req, res) {
+  res.send('Hello Pluton, Maria Jose\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
